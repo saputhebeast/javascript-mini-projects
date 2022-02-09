bab166ba2384b978e09f807cebbf3f491f2c657a
saputhebeast <sapumalwijekoon4@gmail.com>
2022-02-09 20:34:03 +0530

    countdown

---
+++ 01_countdown_timer/script.js
@@ -0,0 +1,30 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const newYear = '1 Jan 2023';
+
+function countdown(){
+    const newYearsDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds/ 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+    console.log(days, hours, mins, seconds);
+}
+
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time;
+}
+
+setInterval(countdown, 1000);
